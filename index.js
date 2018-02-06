a65602f4b5fd6d8a5ba57c5be52183ad9fa6cd76
Marcin Mieszek <marcin.mieszek@gmail.com>
2018-02-06 14:25:14 +0100

    Display Hello World Message

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { Text, AppRegistry } from 'react-native';
+import React from 'react';
 
-AppRegistry.registerComponent('SpaceXMissionControl', () => App);
+const Hello = () => <Text>SpaceX Latest Mission</Text>
+
+AppRegistry.registerComponent('SpaceXMissionControl', () => Hello);
