4b22889ecfe6f438adaf0912eea80d35458b9020
Marcin Mieszek <marcin.mieszek@gmail.com>
2018-02-06 15:12:00 +0100

    Display mission patch

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+  }
+  async componentDidMount() {
+    const response = await fetch(
+      'https://api.spacexdata.com/v2/launches/latest'
+    );
+    const data = await response.json()
+    this.setState({
+      missionPatch: data.links.mission_patch,
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-
+        <Image
+          resizeMode='contain'
+          source={{ uri: this.state.missionPatch}}
+          style={styles.patch}
+        />
       </View>
     )
   }
@@ -15,6 +32,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#DEF5FC',
+  },
+  patch: { 
+    width: 200,
+    height:200,
   }
 })
 
