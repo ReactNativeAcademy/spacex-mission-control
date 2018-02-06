50c6274be914a99bd5fe0607a44c134053a7e7fc
Marcin Mieszek <marcin.mieszek@gmail.com>
2018-02-06 14:49:06 +0100

    Create simple Home component

---
+++ app/components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Home = () => <Text>SpaceX Latest Mission</Text>
+
+export default Home
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import { Text, AppRegistry } from 'react-native';
-import React from 'react';
+import Home from './app/components/Home'
 
-const Hello = () => <Text>SpaceX Latest Mission</Text>
-
-AppRegistry.registerComponent('SpaceXMissionControl', () => Hello);
+AppRegistry.registerComponent('SpaceXMissionControl', () => Home);
