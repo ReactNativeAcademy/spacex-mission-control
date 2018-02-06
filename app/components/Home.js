7a46978847d2ccc51b70f75fa24f2f987079e52b
Marcin Mieszek <marcin.mieszek@gmail.com>
2018-02-06 14:54:33 +0100

    Create Home container

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
-const Home = () => <Text>SpaceX Latest Mission</Text>
+class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#DEF5FC',
+  }
+})
 
 export default Home
